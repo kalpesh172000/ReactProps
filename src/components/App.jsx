604c97b3acabbbdf683cd8cd686898507ef986eb
kalpesh <kalpesh200071@gmail.com>
2024-06-19 10:43:12 +0530

    created seperate component and mapped data to component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,21 @@
 import React from "react";
+import contacts from "../contacts";
+import Card from "./Card";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
+      {
+        contacts.map(contact => (
+          <Card 
+           name = {contact.name}
+           imgURL = {contact.imgURL} 
+           phone = {contact.phone}
+           email = {contact.email}
           />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+        ))
+      }
     </div>
   );
 }
